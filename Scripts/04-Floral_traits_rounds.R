ad62523ce2a38bac0c71d630c9d4b772d0e1e86d
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2026-06-01 10:31:21 +0200

    Added a file for the floral traits that were measured before starting an observation round

---
+++ Scripts/04-Floral_traits_rounds.R
@@ -0,0 +1,42 @@
+# Restore the correct versions of the used packages
+renv::restore() 
+
+# Clean the environment to avoid conflicts with other projects or names
+rm(list = ls())
+
+# When a package is used for the first time, also add to lockfile
+# renv::snapshot() # This shows a new library downloaded in manuscript
+# Load the packages that are needed for this project
+library(tidyverse) # Includes ggplot2, dplyr, etc. can also add them separately 
+# load the required packages
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(chemodiv)
+library(corrr)
+library(factoextra)
+library(ggpubr)
+library(purrr)
+library(tibble)
+
+readr::read_csv # This makes a tibble instead of table, for every variable it stores what the type of variable is. It doesn't just stop at the length it can print, which is what table does.
+
+# ---- Exploratory Analysis ----
+
+# Load the data you want to use
+# Load the data file from google drive 
+Phenotype <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrKk4lVr_GFFwaudVT_jG4tLL9LhCNixrmjzVfOHbsHk3y-3YA8C9dtlWfm4QyFoy9Xmhn2AQmr7SY/pub?gid=206224982&single=true&output=csv") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
