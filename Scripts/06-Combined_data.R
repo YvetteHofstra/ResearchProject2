e22acac4934d43040e12791bdccfe2c6374dfcdf
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2026-06-03 14:57:53 +0200

    Created new files to make the combined plots and one for the repotting information

---
+++ Scripts/06-Combined_data.R
@@ -0,0 +1,71 @@
+# Restore the correct versions of the used packages
+renv::restore() 
+
+# Clean the environment to avoid conflicts with other projects or names
+rm(list = ls())
+
+# When a package is used for the first time, also add to lockfile
+# renv::snapshot() # This shows a new library downloaded in manuscript
+# Load the packages that are needed for this project
+library(tidyverse) # Includes ggplot2, dplyr, etc. can also add them separately 
+# load the required packages
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(chemodiv)
+library(corrr)
+library(factoextra)
+library(ggpubr)
+library(purrr)
+library(tibble)
+library(MASS)
+library(multcomp)
+library(emmeans)
+
+readr::read_csv # This makes a tibble instead of table, for every variable it stores what the type of variable is. It doesn't just stop at the length it can print, which is what table does.
+
+# ---- Exploratory Analysis ----
+
+# Load the data you want to use
+# Load the data file from google drive 
+Phenotype <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrKk4lVr_GFFwaudVT_jG4tLL9LhCNixrmjzVfOHbsHk3y-3YA8C9dtlWfm4QyFoy9Xmhn2AQmr7SY/pub?gid=206224982&single=true&output=csv") 
+
+Nectar <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTrKk4lVr_GFFwaudVT_jG4tLL9LhCNixrmjzVfOHbsHk3y-3YA8C9dtlWfm4QyFoy9Xmhn2AQmr7SY/pub?gid=217767211&single=true&output=csv") 
+
+Flowers <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSQgacYoLmN4V7eOLdZ4JMNue1B_q67kQHkpzGDWt3DCY8FyHVBW5Ml_TR2rViu7jViE_WXihuuZiRc/pub?gid=0&single=true&output=csv") 
+
+Observations <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRNix7qqZS7cB-KkXmk4Yu7XNvI8uNFhS_ZCfTGwVIziLeXCzH-VlHzEzrndxrzLGgWUj-ssOHRmORV/pub?gid=1102638602&single=true&output=csv")
+
+# Replace all NAs in the entire data frame with 0
+Observations <- Observations |>
+  mutate_all(~ ifelse(is.na(.), 0, .))
+
+# Show the type of data R will treat it as (numeric, character, factor, etc.)
+str(Phenotype)
+str(Nectar)
+str(Flowers)
+str(Observations)
+
+# Make numeric instead of integer
+Phenotype$Number_inflorescences <- as.numeric(Phenotype$Number_inflorescences)
+Phenotype$Number_flowers <- as.numeric(Phenotype$Number_flowers)
+Flowers$Number_Inflorescences <- as.numeric(Flowers$Number_Inflorescences)
+
+# Make an exploratory graph
+ggplot(Phenotype, aes(x = , y = )) +
+  geom_point() +
+  labs(title = "Medicago sativa",
+       x = "",
+       y = "Inflorescence (#)") +
+  theme_minimal()
+# Save
+# ggsave("Graphs/.png", width = 8, height = 6, dpi = 300)
+
+
+
+
+
+
+
+
+
