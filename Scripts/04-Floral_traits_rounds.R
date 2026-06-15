6e07a094840c1f6fd2a6188e215697c42b6ba25b
Yvette Hofstra <y.j.hofstra@student.rug.nl>
2026-06-15 11:59:43 +0200

    Tried to change the flowering date plot to show flowering as timeline, but not working yet

--- Scripts/04-Floral_traits_rounds.R
+++ Scripts/04-Floral_traits_rounds.R
@@ -180,7 +180,17 @@ ggplot(Flowering_date, aes(x = Treatment_worded, y = Date, fill = Treatment_word
 # As seen, the date is not looking nice in these plots. Change to as.date
 Flowering_date$Date <- as.Date(Flowering_date$Date)
 
+Dates <- data.frame(aes(Flowering_date),
+  Date = as.Date("06-05-2026", "13-05-2026", "20-05-2026", "22-05-2026", "01-06-2026", "03-06-2026", "09-06-2026", "12-06-2026"))
 
+ggplot(Dates, aes(x = Treatment_worded, y = Date, fill = Treatment_worded)) +
+  geom_boxplot() +
+  facet_wrap(~ Cultivar) +
+  labs(title = "Flowering timing of Medicago sativa",
+       x = "Treatment",
+       y = "Flowering since",
+       fill = "Treatment") +
+  theme_minimal()
 
 
 
